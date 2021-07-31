7ef719ed76b798c67d34e9648ef15095ac81094d
Skye M <skyemc608@gmail.com>
2021-07-31 11:19:57 -0500

    moved logic for questions into functions

--- js/app.js
+++ js/app.js
@@ -1,88 +1,85 @@
-"use strict";
-
-// Prompt the user for their name
 function playGame() {
-  let userName = prompt("Hello, before we get started, please enter your name!");
-  // Return the user's name in a custom welcome greeting
-  alert("Welcome, " + userName + "! Thanks for visiting my page!");
-  alert("If you are ready to start the game press OK to proceed to the rules!");
-  alert("I am going to ask you a variety of questions about myself, all you need to do is try your best to guess the correct answer by entering either 'yes' or 'no'. Press OK to begin!");
-  // Ask the first question
-  let petQuestion = prompt("Is it true that I own a pet cat? I'll give you a hint this time, I only have one pet!").toLowerCase();
-  // Alert user to let them know if they are correct or not
-  if (petQuestion === 'no'.toLowerCase()) {
-    alert("Great job!! You are correct, I have a dog, not a cat.");
-   }
-  else {
-    alert("Woops, incorrect answer!");
-  }
-  // Ask second question
-  let contestQuestion = prompt("Is it true that I won first place in a bellyflop contest in the third grade?").toLowerCase();
-  // Alert user to let them know if they are correct or not
-  if (contestQuestion === 'yes'.toLowerCase()) {
-    alert("Nice, your answer is correct. I even won a trophy for it!");
-    
-  }
-  else {
-    alert("Sorry, that's incorrect!");
-  }
-  // Ask third question 
-  let gardenQuestion = prompt("Is it true that I like growing flowers in my garden?").toLowerCase();
-  // Alert user to let them know if they are correct or not
-  if (gardenQuestion === 'no'.toLowerCase()) {
-    alert("Correct! I love flowers but I suck at growing them. However, I do grow herbs and veggies in my garden!");
-  }
-  else {
-    alert("Nope! Your answer is incorrect");
-  }
-  // Ask fourth question
-  let nameQuestion = prompt("Is it true that I was named after the Isle of Skye which is located in New Zealand?").toLowerCase();
-  // Alert user to let them know if they are correct or not
-  if (nameQuestion === 'no'.toLowerCase()) {
-    alert("Wow, that is correct! Looks like you're too smart for my little tricks. I was named after the Isle of Skye but it is actually located in Scotland");
-    
-  }
-  else {
-    alert("Sorry you are incorrect on a technicality due to the fact that the Isle of Skye is in Scotland, not New Zealand! Gotcha!!");
-  }
-  // Ask last question
-  let hometownQuestion = prompt("Is it true that I was born in LaCrosse, Wisconsin?").toLowerCase();
-  // Alert the user to let them know if they are correct or not
-  if (hometownQuestion === 'yes'.toLowerCase()) {
-    alert("You are correct, it is indeed true! Cheesehead represent!!");
-    
-  }
-  else {
-    alert("That is incorrect, better luck next time!");
-  }
-  let myAge = "24";
+  let name = prompt("Hello, before we get started, please enter your name!");
 
-  alert("Okay, let's see if you can guess my age! I'll give you 4 attempts. Select OK to give it a try");
+   // Return the user's name in a custom welcome greeting
+   alert("Welcome, " + name + "! Thanks for visiting my page!");
+   alert("If you are ready to start the game press OK to proceed to the rules!");
+   alert("I am going to ask you a variety of questions about myself, all you need to do is try your best to guess the correct answer by entering either 'yes' or 'no'. Press OK to begin!");
 
-  for (let x = 0; x < 4; x++) {
+  // count how many questions the user answered correctly
+  let correctGuesses = 0;
 
-    let userGuess = prompt("How old am I?");
+  // store questions in an array
+  const questions = [ 'Do I have a pet cat?', 'Do I like to grow flowers in my garden?', 'Did I win first place in a belly flop contest in the third grade?', 'Was I named after a place in New Zealand called The Isle of Skye?', 'Was I born in LaCrosse Wisconsin?', 'Lets see if you can guess my age! How old am I?', 'Can you guess one of my favorite instruments?' ];
 
+  for (let i = 0; i < questions.length; i++) {
+    // normalize answers to easily verify input
+    let guess = prompt(questions[i]).toLowerCase();
+    if (i === 6) {
+      const instruments = ['Electric Violin', 'Theremin', 'Xylophone', 'Upright Bass', 'Electric Guitar', 'Mandolin'];
 
-    if (userGuess > myAge) {
-
-      alert("Incorrect!! that's too high!");
-
+      let attempts = 6;
+      while (attempts !== 0) {
+        // check if input matches any instruments in the array
+        if (instruments.includes(guess)) {
+          alert(`Correct!\nYou could've entered any instrument in this list:\n${instruments}`);
+          correctGuesses++;
+          attempts = 0;
+        } else {
+          attempts--;
+          if (attempts === 0) {
+            alert(`Sorry! It looks like you're out of attempts.\nAny of the following instruments would have been correct: ${instruments}`);
+          } else {
+            guess = prompt(`Sorry, that's not one of them. Please try again.\n${questions[i]}\nAttempts left: ${attempts}`);
+          }
+        }
+      }
     }
-
-    if (userGuess < myAge) {
-      alert("That is incorrect, your number is too low!");
+    else if (i === 5) {
+      // Guess my age
+      let myAge = "24";
+      let attempts = 4;
+      while(attempts !== 0) {
+        if (guess === myAge) {
+          alert("Correct!");
+          correctGuesses++;
+          attempts = 0;
+        } else {
+          attempts--;
+          if (attempts === 0) {
+            alert(`Uh-oh! Looks like you're out of attempts. Im sorry, the correct answer is ${myAge}.`);
+          } else if (guess > myAge) {
+            guess = prompt(`Incorrect, that number is too high. Please try again.\n${questions[i]}\nAttempts left: ${attempts}`);
+          } else {
+            guess = prompt(`Incorrect,, that number is too low. Please try again.\n${questions[i]}\nAttempts left: ${attempts}`);
+          }
+        }
+      }
     }
-
-    if (userGuess === "23") {
-      alert("Sorry, that's incorrect....but you're VERY close!!");
-    }
-    if (userGuess === myAge) {
-      alert("That is correct, great job!");
-      break;
+    // for questions 3 & 5, the correct answer is yes. compare to user input. if correct, increment correctGuesses by 1.
+    else if (i === 2 || i === 4) {
+      if (guess === 'yes') {
+        alert("Correct!");
+        correctGuesses++;
+      } else {
+        alert("Incorrect!");
+      }
+    } else {
+      // for all other questions, the proper response is no. compare to user input. if correct, increment correctGuesses by 1.
+      if (guess === 'no') {
+        alert("Correct!");
+        correctGuesses++;
+      } else {
+        alert("Incorrect!");
+      }
     }
   }
 
-  alert("I am 24 years old");
+  // Thank user for participation & tell user how many questions they answered correctly
+  alert(`Thanks for taking time to play my game today, ${name[0].toUpperCase() + name.substring(1).toLowerCase()}! I hope you had fun and learned a little bit about me. Your score is ${correctGuesses} out of 7.`);
+
 }
 
+// call quiz function when button is clicked
+let button = document.querySelector('button');
+button.addEventListener('click', quiz);
